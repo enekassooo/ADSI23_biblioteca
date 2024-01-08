b72c22f090b1e744ceb24aa8309725e74d7118f8
00sergio <sergigonza72@gmail.com>
2024-01-08 11:54:16 +0100

    admin

--- controller/LibraryController.py
+++ controller/LibraryController.py
@@ -5,78 +5,89 @@ from model.tools import hash_password
 
 db = Connection()
 
+
 class LibraryController:
-	__instance = None
+    __instance = None
 
-	def __new__(cls):
-		if cls.__instance is None:
-			cls.__instance = super(LibraryController, cls).__new__(cls)
-			cls.__instance.__initialized = False
-		return cls.__instance
+    def _new_(cls):
+        if cls.__instance is None:
+            cls._instance = super(LibraryController, cls).__new_(cls)
+            cls.__instance.__initialized = False
+        return cls.__instance
 
-
-	def search_books(self, title="", author="", limit=6, page=0):
-		count = db.select("""
+    def search_books(self, title="", author="", limit=6, page=0):
+        count = db.select("""
 				SELECT count() 
 				FROM Book b, Author a 
 				WHERE b.author=a.id 
 					AND b.title LIKE ? 
 					AND a.name LIKE ? 
 		""", (f"%{title}%", f"%{author}%"))[0][0]
-		res = db.select("""
+        res = db.select("""
 				SELECT b.* 
 				FROM Book b, Author a 
 				WHERE b.author=a.id 
 					AND b.title LIKE ? 
 					AND a.name LIKE ? 
 				LIMIT ? OFFSET ?
-		""", (f"%{title}%", f"%{author}%", limit, limit*page))
-		books = [
-			Book(b[0],b[1],b[2],b[3],b[4])
-			for b in res
-		]
-		return books, count
+		""", (f"%{title}%", f"%{author}%", limit, limit * page))
+        books = [
+            Book(b[0], b[1], b[2], b[3], b[4])
+            for b in res
+        ]
+        return books, count
 
-	def get_user(self, email, password):
-		user = db.select("SELECT * from User WHERE email = ? AND password = ?", (email, hash_password(password)))
-		if len(user) > 0:
-			return User(user[0][0], user[0][1], user[0][2])
-		else:
-			return None
+    def get_user(self, email, password):
+        user = db.select("SELECT * from User WHERE email = ? AND password = ?", (email, hash_password(password)))
+        if len(user) > 0:
+            return User(user[0][0], user[0][1], user[0][2])
+        else:
+            return None
 
-	def get_user_cookies(self, token, time):
-		user = db.select("SELECT u.* from User u, Session s WHERE u.id = s.user_id AND s.last_login = ? AND s.session_hash = ?", (time, token))
-		if len(user) > 0:
-			return User(user[0][0], user[0][1], user[0][2])
-		else:
-			return None
-	def get_recomendaciones(self, user):
-		librosleidos = db.select("SELECT book_id FROM User_Book WHERE user_id = ?", (user,))
-		recomendaciones = []
-		autores_leidos = db.select("SELECT DISTINCT author FROM Book WHERE id IN (SELECT book_id FROM User_Book WHERE user_id = ?)", (user,))
-		for autor in autores_leidos:
-			libros_recomendados = db.select("SELECT * FROM Book WHERE author = ?", (autor[0],))
-			for libro in libros_recomendados:
-				autor_info = db.select("SELECT * FROM Author WHERE id = ?", (libro[2],))
-				libro_info_list = list(libro)
-				libro_info_list.append(autor_info[0][1])
-				libro_info = tuple(libro_info_list)
-				recomendaciones.append(libro_info)
-			
-		return recomendaciones
+    def get_user_cookies(self, token, time):
+        user = db.select(
+            "SELECT u.* from User u, Session s WHERE u.id = s.user_id AND s.last_login = ? AND s.session_hash = ?",
+            (time, token))
+        if len(user) > 0:
+            return User(user[0][0], user[0][1], user[0][2])
+        else:
+            return None
 
-	def añadir_usuario(self):
-		nombre = request.values.get("nombre", "")
-		email = request.values.get("email", "")
-		contrasena = request.values.get("contraseña", "")
-		db.insert("INSERT INTO User(name, email, password) VALUES (?, ?, ?)", (nombre, email, contrasena))
-		return User(nombre, email, contrasena)
+    def get_recomendaciones(self, user):
+        librosleidos = db.select("SELECT book_id FROM User_Book WHERE user_id = ?", (user,))
+        recomendaciones = []
+        autores_leidos = db.select(
+            "SELECT DISTINCT author FROM Book WHERE id IN (SELECT book_id FROM User_Book WHERE user_id = ?)", (user,))
+        for autor in autores_leidos:
+            libros_recomendados = db.select("SELECT * FROM Book WHERE author = ?", (autor[0],))
+            for libro in libros_recomendados:
+                autor_info = db.select("SELECT * FROM Author WHERE id = ?", (libro[2],))
+                libro_info_list = list(libro)
+                libro_info_list.append(autor_info[0][1])
+                libro_info = tuple(libro_info_list)
+                recomendaciones.append(libro_info)
 
-	def añadir_libro(self):
-		titulo = request.values.get("titulo", "")
-		autor = request.values.get("autor", "")
-		portada = request.values.get("portada", "")
-		desc = request.values.get("desc", "")
-		db.insert("INSERT INTO Book(title, author, cover, description) VALUES (?, ?, ?, ?)", (titulo, autor, portada, desc))
-		return Book(titulo, autor, portada, desc)
+        return recomendaciones
 
+    def añadir_usuario(self, nombre, email, contraseña):
+        db.insert("INSERT INTO User(name, email, password) VALUES (?, ?, ?)", (nombre, email, contraseña))
+        return
+
+    def eliminar_usuario(self, userid):
+        db.delete("DELETE FROM User WHERE id = ?", (userid,))
+        print(f"User con ID {userid} eliminado correctamente")
+        return
+    def añadir_libro(self, titulo, autor, portada, desc):
+        db.insert("INSERT INTO Book(title, author, cover, description) VALUES (?, ?, ?, ?)",
+                  (titulo, autor, portada, desc))
+        return
+
+    def eliminar_libro(self, libroid):
+        db.delete("DELETE FROM Book WHERE id = ?", (libroid,))
+        print(f"Libro con ID {libroid} eliminado correctamente")
+        return
+
+    def es_admin(self, user_id):
+        result = db.select("SELECT * FROM User u JOIN Admin a ON u.id = a.user_id WHERE u.id = ?", (user_id,))
+        es_administrador = bool(result)
+        return es_administrador
--- controller/webServer.py
+++ controller/webServer.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, make_response, redirect, g
 
 from controller.LibraryController import LibraryController
@@ -6,110 +5,132 @@ from model import Connection, Book, User
 
 app = Flask(__name__, static_url_path='', static_folder='../view/static', template_folder='../view/')
 
-
 library = LibraryController()
 
 
 @app.before_request
 def get_logged_user():
-	if '/css' not in request.path and '/js' not in request.path:
-		token = request.cookies.get('token')
-		time = request.cookies.get('time')
-		if token and time:
-			request.user = library.get_user_cookies(token, float(time))
-			if request.user:
-				request.user.token = token
-				g.user = request.user
+    if '/css' not in request.path and '/js' not in request.path:
+        token = request.cookies.get('token')
+        time = request.cookies.get('time')
+        if token and time:
+            request.user = library.get_user_cookies(token, float(time))
+            if request.user:
+                request.user.token = token
+                g.user = request.user
 
 
 @app.after_request
 def add_cookies(response):
-	if 'user' in dir(request) and request.user and request.user.token:
-		session = request.user.validate_session(request.user.token)
-		response.set_cookie('token', session.hash)
-		response.set_cookie('time', str(session.time))
-	return response
+    if 'user' in dir(request) and request.user and request.user.token:
+        session = request.user.validate_session(request.user.token)
+        response.set_cookie('token', session.hash)
+        response.set_cookie('time', str(session.time))
+    return response
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    user = getattr(request, 'user', None)
+    if user is None:
+        return render_template('index.html')
+
+    admin = library.es_admin(user.id)
+    if admin:
+        return render_template('indexadmin.html')
+    else:
+        return render_template('index.html')
 
 
 @app.route('/catalogue')
 def catalogue():
-	title = request.values.get("title", "")
-	author = request.values.get("author", "")
-	page = int(request.values.get("page", 1))
-	books, nb_books = library.search_books(title=title, author=author, page=page - 1)
-	total_pages = (nb_books // 6) + 1
-	return render_template('catalogue.html', books=books, title=title, author=author, current_page=page,
-	                       total_pages=total_pages, max=max, min=min)
+    title = request.values.get("title", "")
+    author = request.values.get("author", "")
+    page = int(request.values.get("page", 1))
+    books, nb_books = library.search_books(title=title, author=author, page=page - 1)
+    total_pages = (nb_books // 6) + 1
+    return render_template('catalogue.html', books=books, title=title, author=author, current_page=page,
+                           total_pages=total_pages, max=max, min=min)
+
 
 @app.route('/recomendaciones')
 def recomendaciones():
-	user = request.user.identity()
-	print("El usuario es:", user)
-	recomended_books = library.get_recomendaciones(user)
-	print("Recomendaciones:", recomended_books)
-	return render_template('recomendaiones.html', rbooks=recomended_books)
+    user = request.user.identity()
+    print("El usuario es:", user)
+    recomended_books = library.get_recomendaciones(user)
+    print("Recomendaciones:", recomended_books)
+    return render_template('recomendaiones.html', rbooks=recomended_books)
 
 
-def admin():
-
-	user_id = request.user.identity()
-
-
-	query = "SELECT * FROM User u JOIN Admin a ON u.id = a.user_id WHERE u.id = ?"
-	result = db.select_one(query, (user_id,))
-
-
-	es_administrador = result is not None
-
-	return render_template('index.html', es_administrador=es_administrador)
-@app.route('/gestionLibros')
+@app.route('/gestionLibros', methods=['GET', 'POST'])
 def gestionLibros():
-	libro = library.añadir_libro()
-	print("Se ha añadido el usuario correctamente")
-	return render_template('gestionlibros.html', book=libro)
+    if request.method == 'POST':
+        libroid = request.form.get("id", "")
+        if libroid:
+            library.eliminar_libro(libroid)
+            print(f"Se ha eliminado el libro con ID {libroid}")
+        else:
+            print(f"No se va a borrar un libro")
+
+        titulo = request.form.get("titulo", "")
+        autor = request.form.get("autor", "")
+        portada = request.form.get("portada", "")
+        desc = request.form.get("descripcion", "")
+        if titulo and autor and desc:
+            library.añadir_libro(titulo, autor, portada, desc)
+            print("Se ha añadido el libro correctamente")
+
+    return render_template('gestionlibros.html')
 
 
-@app.route('/gestionUsuarios')
+@app.route('/gestionusuarios', methods=['GET', 'POST'])
 def gestionUsuarios():
-	usuario = library.añadir_usuario()
-	print("Se ha añadido el usuario correctamente")
-	return render_template('gestionUsuarios.html', user=usuario)
+    if request.method == 'POST':
+        userid = request.form.get("id", "")
+        if userid:
+            library.eliminar_usuario(userid)
+            print(f"Se ha eliminado el usuario con ID {userid}")
+        else:
+            print(f"No se va a borrar el usuario")
 
+        nombre = request.form.get("nombre", "")
+        email = request.form.get("email", "")
+        contraseña = request.form.get("contraseña", "")
+        if nombre and email and contraseña:
+            library.añadir_usuario(nombre, email, contraseña)
+            print("Se ha añadido el usuario correctamente")
+
+    return render_template('gestorusuarios.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if 'user' in dir(request) and request.user and request.user.token:
-		return redirect('/')
-	email = request.values.get("email", "")
-	password = request.values.get("password", "")
-	user = library.get_user(email, password)
-	if user:
-		session = user.new_session()
-		resp = redirect("/")
-		resp.set_cookie('token', session.hash)
-		resp.set_cookie('time', str(session.time))
-	else:
-		if request.method == 'POST':
-			return redirect('/login')
-		else:
-			resp = render_template('login.html')
-	return resp
+    if 'user' in dir(request) and request.user and request.user.token:
+        return redirect('/')
+    email = request.values.get("email", "")
+    password = request.values.get("password", "")
+    user = library.get_user(email, password)
+    if user:
+        session = user.new_session()
+        resp = redirect("/")
+        resp.set_cookie('token', session.hash)
+        resp.set_cookie('time', str(session.time))
+    else:
+        if request.method == 'POST':
+            return redirect('/login')
+        else:
+            resp = render_template('login.html')
+    return resp
 
 
 @app.route('/logout')
 def logout():
-	path = request.values.get("path", "/")
-	resp = redirect(path)
-	resp.delete_cookie('token')
-	resp.delete_cookie('time')
-	if 'user' in dir(request) and request.user and request.user.token:
-		request.user.delete_session(request.user.token)
-		request.user = None
-		resp = render_template('index.html')
-	return resp
+    path = request.values.get("path", "/")
+    resp = redirect(path)
+    resp.delete_cookie('token')
+    resp.delete_cookie('time')
+    if 'user' in dir(request) and request.user and request.user.token:
+        request.user.delete_session(request.user.token)
+        request.user = None
+        resp = render_template('index.html')
+    return resp
